424d2a0e18bbfa988b9759c0f82cf7675daf6ff8
George Hill <george.fxmediaweb@gmail.com>
2021-08-23 00:06:21 +0700

    update api prodution

--- src/components/ultilities/constanst.tsx
+++ src/components/ultilities/constanst.tsx
@@ -1,5 +1,5 @@
 export const apiUrl =
     process.env.NODE !== "production"
         ? "http://localhost:4000/api"
-        : "http://ec2-13-229-153-126.ap-southeast-1.compute.amazonaws.com/";
+        : "http://ec2-18-140-197-50.ap-southeast-1.compute.amazonaws.com/api";
 export const LOCAL_STORAGE_TOKEN_NAME = "george-mern";
