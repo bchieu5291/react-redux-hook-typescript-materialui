52192410408e2ff1bf792be0e23611d177eb5005
George Hill <george.fxmediaweb@gmail.com>
2021-08-23 00:09:25 +0700

    ec 2

--- src/components/ultilities/constanst.tsx
+++ src/components/ultilities/constanst.tsx
@@ -1,5 +1,5 @@
 export const apiUrl =
     process.env.NODE !== "production"
-        ? "http://localhost:4000/api"
-        : "http://ec2-18-140-197-50.ap-southeast-1.compute.amazonaws.com/api";
+        ? "http://ec2-18-140-197-50.ap-southeast-1.compute.amazonaws.com/api"
+        : "http://localhost:4000/api";
 export const LOCAL_STORAGE_TOKEN_NAME = "george-mern";
