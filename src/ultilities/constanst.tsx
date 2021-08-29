8864d277d56a56d46858e5c7a2bc69e9342af176
George Hill <george.fxmediaweb@gmail.com>
2021-08-29 21:58:39 +0700

    update evn

--- src/ultilities/constanst.tsx
+++ src/ultilities/constanst.tsx
@@ -1,5 +1,5 @@
 export const apiUrl =
     process.env.REACT_APP_SITE_ENV !== "production"
         ? "http://localhost:4001/api"
-        : "http://ec2-18-140-197-50.ap-southeast-1.compute.amazonaws.com/api";
+        : process.env.REACT_APP_PROD_API_URL;
 export const LOCAL_STORAGE_TOKEN_NAME = "george-mern";
